1e0d66229fca4961b8f7187fdc3c56d43ad92974
Himanshu0ar <dubeyhimanshu0ar@gmail.com>
2025-11-28 12:45:50 +0530

    Delete Packages directory

--- Packages/src/com/university/department/cse/Course.java
+++
@@ -1,9 +0,0 @@
-package com.university.department.cse;
-
-public class Course {
-    public void displayCourse() {
-        System.out.println("Course: B.Tech CSE");
-        System.out.println("Department: Computer Science & Engineering");
-        System.out.println("University: XYZ University");
-    }
-}
